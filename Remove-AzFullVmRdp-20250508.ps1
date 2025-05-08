eab561d5f39d336ea8acddc897b353383d1c54a0
Jet Mariano | Cloud & Dev Portfolio <jetnmariano@gmail.com>
2025-05-08 11:37:58 -0600

    Delete Remove-AzFullVmRdp-20250508.ps1

--- Remove-AzFullVmRdp-20250508.ps1
+++
@@ -1,7 +0,0 @@
-# Script: Remove-AzFullVmRdp-20250508.ps1
-# Description: Deletes the entire resource group and all resources within it.
-
-$resourceGroup = "JetTestLabRG"
-
-# Remove everything
-Remove-AzResourceGroup -Name $resourceGroup -Force -AsJob
